31110960fc7f437bd6c2c7d091751a649a4608b8
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2023-01-14 16:50:15 +0530

    create: code refactoring

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Navbar } from './shared/Navbar';
+export { default as Search } from './search/Search';
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,5 +1,26 @@
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { Search } from '../../components';
+import { Logo } from '../../assets';
+import theme from '../../theme';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { colors } = theme;
+
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ background: colors.darkBlue }}
+    >
+      <Link>
+        <img src={Logo} height={45} alt="logo" />
+      </Link>
+      <Search />
+    </Stack>
+  );
 };
 
 export default Navbar;
